0189ff72cf7242dbc90103aace832ae7f5f3db45
Chris Naismith <christophernaismith@gmail.com>
2019-06-30 07:04:15 -0400

    Remove strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,18 +28,6 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: "gatsby-source-strapi",
-      options: {
-        apiURL: "http://localhost:1337",
-        contentTypes: [
-          // List of the Content Types you want to be able to request from Gatsby.
-          // "article",
-          // "user",
-        ],
-        queryLimit: 1000,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
